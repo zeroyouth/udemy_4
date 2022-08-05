a5786f916ebe3642210475a18395ff4fa4656acc
youngeun choi <choi46910@naver.com>
2022-08-05 15:33:18 +0900

    Add : 장바구니 버튼

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,15 @@
 import { Fragment } from "react";
 
-import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Zero Youth 식당</h1>
-        <button>장바구니🛒</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="맛있는 음식으로 가득찬 테이블" />
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>내 장바구니</span>
+      <span className={classes.badge}>
+        3
+      </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
