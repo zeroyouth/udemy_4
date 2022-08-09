fb7d7c7b3d2bbdb5e7a34c1379152dc1583bb7e5
youngeun choi <choi46910@naver.com>
2022-08-09 14:40:50 +0900

    Add : MealItem 추가

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,40 +1,52 @@
 import React from 'react';
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
+    name: '초밥',
+    description: '신선한 생선살과 따근한 밥의 조화',
     price: 22.99,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
+    name: '타코',
+    description: '바삭한 타코 맛보세요',
     price: 16.5,
   },
   {
     id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
+    name: '햄버거',
+    description: '참깨빵 위에 순살 고기 패티 두 장..',
     price: 12.99,
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
+    name: '샐러드볼',
+    description: '건강한 음식을 찾으신다면 선택하세요',
     price: 18.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>
+          {mealsList}
+        </ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`; //소수점 이하 두 자리까지 표시
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.price}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
