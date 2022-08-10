da334d0eed6639d3a15d8a2b8caa14b1f52a7109
youngeun choi <choi46910@naver.com>
2022-08-10 23:41:47 +0900

    Add : MealItemForm 추가

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input label="수량" input={{
+        id: 'amount',
+        type: 'number',
+        min: '1',
+        max: '5',
+        step: '1',
+        defaultValue: '1'
+      }}
+      />
+      <button>+ 추가</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
