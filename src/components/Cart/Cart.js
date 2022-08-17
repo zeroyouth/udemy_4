415b47b6f45c512cbb5a7df943f1e0626590bc62
youngeun choi <choi46910@naver.com>
2022-08-17 13:15:31 +0900

    Add : 장바구니에 음식 담기

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,29 @@
+import { useContext } from 'react';
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0; //장바구니 컨텍스트 항목의 길이가 0보다 큰지 확인
+
+  const cartItemRemoveHandler = id => { };
+
+  const cartItemAddHandler = item => { };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: '초밥', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.apply(null, item)} />
       ))}
     </ul>
   );
@@ -15,13 +33,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>총 금액</span>
-        <span>$ 35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           닫기
         </button>
-        <button className={classes.button}>주문</button>
+        {hasItems && <button className={classes.button}>주문</button>}
       </div>
     </Modal>
   );
